5bf89ffa4a7d63a1b43d10ef9844d67f37b15562
Ub <nisargvora2104@gmail.com>
2020-10-21 15:00:05 +0530

    minor chjanges

--- lexerVars.h
+++ lexerVars.h
@@ -1,23 +1,23 @@
 #include<stdio.h>
 #define MAX_VAR_NAME_LEN 50
-struct TOKEN
-{
-    union{
-        char lexeme[MAX_VAR_NAME_LEN];
-        int int_val;
-        double real_val;
-    };
-    
-    int line_num;
-    token token_name;
-};
-
 
 typedef enum {
-    #include "tokens.txt"
-} token;
+    integer,real
+} terminal;
 
 typedef enum{
-    #include "non_terminals.txt"
+    JAGGED_LIST
 } non_terminal;
+
+
+struct TOKEN
+{
+    char lexeme[MAX_VAR_NAME_LEN];
+    int line_num;
+    terminal token_name;
+    struct TOKEN* nextToken;
+};
+typedef struct TOKEN token
+
+
 

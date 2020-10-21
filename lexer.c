11af19ac9424a4238a03f857e6633ad017bc3ab4
PSK1103 <prathamkatiyar11@gmail.com>
2020-10-21 16:51:30 +0530

    termainal val enum returned

--- lexer.c
+++ lexer.c
@@ -46,20 +46,61 @@ int main() {
 terminal returnTerminalVal(char* lex) {
     if(!strcmp(lex,"program"))
         return program;
-    else if(!strcmp(lex,"rbs"))
+    else if(!strcmp(lex,"()"))
         return rbs;
-    else if(!strcmp(lex,"cb_op"))
+    else if(!strcmp(lex,"{"))
         return cb_op;
-    else if(!strcmp(lex,"cb_cl"))
+    else if(!strcmp(lex,"}"))
         return cb_cl;
     else if(!strcmp(lex,"declare"))
         return declare;
-    else if(!strcmp(lex,"id"))
-        return id;
-    else if(!strcmp(lex,"col"))
+    else if(!strcmp(lex,":"))
         return col;
     else if(!strcmp(lex,"list"))
         return list;
     else if(!strcmp(lex,"of"))
-        return
+        return of;
+    else if(!strcmp(lex,"variables"))
+        return variables;
+    else if(!strcmp(lex,";"))
+        return semicol;
+    else if(!strcmp(lex,"integer"))
+        return integer;
+    else if(!strcmp(lex,"boolean"))
+        return boolean;
+    else if(!strcmp(lex,"real"))
+        return real;
+    else if(!strcmp(lex,"array"))
+        return array;
+    else if(!strcmp(lex,"jagged"))
+        return jagged;
+    else if(!strcmp(lex,".."))
+        return ddot;
+    else if(!strcmp(lex,"R1"))
+        return R1;
+    else if(!strcmp(lex,"["))
+        return sq_op;
+    else if(!strcmp(lex,"]"))
+        return sq_cl;
+    else if(!strcmp(lex,"size"))
+        return size;
+    else if(!strcmp(lex,"values"))
+        return values;
+    else if(!strcmp(lex,"+"))
+        return add_op;
+    else if(!strcmp(lex,"-"))
+        return sub_op;
+    else if(!strcmp(lex,"*"))
+        return mul_op;
+    else if(!strcmp(lex,"/"))
+        return div_op;
+
+    for(int i=0;i<strlen(lex);i++) {
+
+        if(lex[i]-'0' < 0 || lex[i]-'0' > 9) {
+            return id;
+        }
+    }
+    return num;
+
 }

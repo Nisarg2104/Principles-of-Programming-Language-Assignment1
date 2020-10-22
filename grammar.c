c7fed0145c7b576cc726d1e67ec1c246292f24f7
Kalash Shah <kalashshah11@gmail.com>
2020-10-22 19:28:01 +0530

    Parse Table Done

--- grammar.c
+++ grammar.c
@@ -52,62 +52,108 @@ int ** hardCodedRules()
         memset(p[i], -1, 31*sizeof(int));
     }
 
-    p[1][7] = 1;
-    p[2][7] = 4;
-    p[3][7] = 5;
-    p[4][7] = 7;
-    p[16][7] = 25;
-    p[20][7] = 31;
-    p[27][7] = 41;
-    p[28][7] = 43;
-    p[29][7] = 44;
-    p[30][7] = 46;
-    p[31][7] = 47;
-    p[32][7] = 48;
-    p[33][7] = 50;
-    p[35][7] = 54;
-    p[36][7] = 56;
-    p[38][7] = 60;
-    p[17][8] = 26;
-    p[21][8] = 32;
-    p[23][8] = 35;
-    p[24][8] = 37;
-    p[27][8] = 42;
-    p[32][8] = 48;
-    p[35][8] = 54;
-    p[11][11] = 18;
-    p[12][11] = 20;
-    p[14][11] = 22;
-    p[25][11] = 38;
-    p[26][11] = 39;
-    p[16][12] = 25;
-    p[18][12] = 28;
-    p[20][12] = 31;
-    p[22][12] = 33;
-    p[24][12] = 36;
-    p[38][12] = 60;
-    p[1][13] = 2;
-    p[16][13] = 25;
-    p[18][13] = 27;
-    p[20][13] = 31;
-    p[22][13] = 34;
-    p[24][13] = 36;
-    p[33][13] = 50;
-    p[36][13] = 56;
-    p[38][13] = 60;
-    p[30][16] = 45;
-    p[38][16] = 60;
-
-
     return p;
 }
 void initialiseRules(int **rules)
 {
     rules[PROGRAM][program] = 0;
+    rules[STATEMENTS][id] = 1;
+    rules[STATEMENTS][declare] = 1;
+    rules[STATEMENTS][cb_cl] = 2;
+    rules[STATEMENT][declare] = 3;
+    rules[STATEMENT][id] = 4;
+    rules[DECLARE_STATEMENT][id] = 5;
+    rules[DECLARE_STATEMENT][list] = 6;
+    rules[SINGLE_DECLARE][id] = 7;
+    rules[MULTI_DECLARE][list] = 8;
+    rules[TYPE][integer] = 9;
+    rules[TYPE][real] = 9;
+    rules[TYPE][boolean] = 9;
+    rules[TYPE][array] = 10;
+    rules[TYPE][jagged] = 10;
+    rules[ARRAY][array] = 11;
+    rules[ARRAY][jagged] = 12;
+    rules[PRIM_TYPE][integer] = 13;
+    rules[PRIM_TYPE][boolean] = 14;
+    rules[PRIM_TYPE][real] = 15;
+    rules[RECTANGULAR][array] = 16;
+    rules[JAGGED][jagged] = 17;
+    rules[JAGGED_INIT][sq_op] = 18;
+    rules[JAGGED_INIT_DASH][of] = 19;
+    rules[JAGGED_INIT_DASH][sq_op] = 20;
+    rules[TD_INIT][of] = 21;
+    rules[THD_INIT][sq_op] = 22;
+    rules[TD_VALS][r1] = 23;
+    rules[TD_VALS_DASH][r1] = 24;
+    rules[TD_VALS_DASH][id] = 25;
+    rules[TD_VALS_DASH][semicol] = 25;
+    rules[TD_VALS_DASH][cb_cl] = 25;
+    rules[TD_VALS_DASH][declare] = 25;
+    rules[INT_LIST][num] = 26;
+    rules[INT_LIST_DASH][cb_cl] = 27;
+    rules[INT_LIST_DASH][semicol] = 28;
+    rules[THD_VALS][r1] = 29;
+    rules[THD_VALS_DASH][r1] = 30;
+    rules[THD_VALS_DASH][id] = 31;
+    rules[THD_VALS_DASH][semicol] = 31;
+    rules[THD_VALS_DASH][cb_cl] = 31;
+    rules[THD_VALS_DASH][declare] = 31;
+    rules[INT_LIST_LIST][num] = 32;
+    rules[INT_LIST_LIST_DASH][cb_cl] = 34;
+    rules[INT_LIST_LIST_DASH][semicol] = 33;
+    rules[INT_VAR_LIST][num] = 35;
+    rules[INT_VAR_LIST_DASH][cb_cl] = 36;
+    rules[INT_VAR_LIST_DASH][semicol] = 36;
+    rules[INT_VAR_LIST_DASH][num] = 37;
+    rules[ARRAY_RANGE][sq_op] = 38;
+    rules[ARRAY_RANGE_DASH][sq_op] = 39;
+    rules[ARRAY_RANGE_DASH][of] = 40;
+    rules[IDX][id] = 41;
+    rules[IDX][num] = 42;
+    rules[VAR_LIST][id] = 43;
+    rules[MULT_ID][id] = 44;
+    rules[MULT_ID_DASH][col] = 45;
+    rules[MULT_ID_DASH][id] = 46;
+    rules[ASSIGN_STATEMENT][id] = 47;
+    rules[EXPR][id] = 48;
+    rules[EXPR][num] = 48;
+    rules[EXPR_DASH][or_op] = 49;
+    rules[EXPR_DASH][add_op] = 49;
+    rules[EXPR_DASH][sub_op] = 49;
+    rules[EXPR_DASH][id] = 50;
+    rules[EXPR_DASH][cb_cl] = 50;
+    rules[EXPR_DASH][declare] = 50;
+    rules[ASO_OP][or_op] = 53;
+    rules[ASO_OP][sub_op] = 52;
+    rules[ASO_OP][add_op] = 51;
+    rules[TERM][id] = 54;
+    rules[TERM][num] = 54;
+    rules[TERM_DASH][and_op] = 55;
+    rules[TERM_DASH][mul_op] = 55;
+    rules[TERM_DASH][div_op] = 55;
+    rules[TERM_DASH][or_op] = 56;
+    rules[TERM_DASH][sub_op] = 56;
+    rules[TERM_DASH][add_op] = 56;
+    rules[TERM_DASH][id] = 56;
+    rules[TERM_DASH][cb_cl] = 56;
+    rules[TERM_DASH][declare] = 56;
+    rules[MDA_OP][and_op] = 59;
+    rules[MDA_OP][mul_op] = 58;
+    rules[MDA_OP][div_op] = 57;
+    rules[EPS][or_op] = 60;
+    rules[EPS][sub_op] = 60;
+    rules[EPS][add_op] = 60;
+    rules[EPS][cb_cl] = 60;
+    rules[EPS][id] = 60;
+    rules[EPS][semicol] = 60;
+    rules[EPS][declare] = 60;
+    rules[EPS][col] = 60;   
+
+
     for(int i = 0;i<39;i++)
     {
         for(int j = 0;j<31;j++)
-            printf("%d ",PROGRAM);
+            printf("%d ",rules[i][j]);
         printf("\n");
     }
 }
